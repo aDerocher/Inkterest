36fc4b55f0f3c068027b1f07817081db2354a65f
Andrew Derocher <am.derocher@gmail.com>
2021-10-09 17:34:11 -0700

    improvements finished for users

--- python-project-starter-main/app/seeds/SEED_GEN.js
+++ python-project-starter-main/app/seeds/SEED_GEN.js
@@ -1,13 +1,13 @@
 
-// will generate users for seed data
+// ===== will generate users for seed data =====
 // will alternate profile pic as man/woman on even/odd numbers
 // there's only 100 valid urls for men/women, this will make 50 of each.
-for (i=1; i < 10; i++){
+for (i=1; i < 30; i++){
     let g = 'men'
     if (i%2 === 0){g = 'women'}
 
     console.log(
-        `demo = User\(
+        `demouser${i} = User\(
             username='user${i}',
             profile_picture='https://randomuser.me/api/portraits/${g}/${i}.jpg',
             password='password',
