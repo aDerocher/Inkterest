9a4c1e3bc25a8f99222c879863802f201de876ad
Alwyn Grant <marzgrant@gmail.com>
2021-10-10 15:02:29 -0700

    finished working on aws post route

---
+++ python-project-starter-main/app/api/ink_routes.py
@@ -0,0 +1,44 @@
+from flask import Blueprint, jsonify, request
+from flask_login import login_required, current_user
+from app.models import db, Ink
+from app.aws import (
+    upload_file_to_s3, allowed_file, get_unique_filename)
+
+ink_routes = Blueprint('inks', __name__)
+
+
+@ink_routes.route('/new-ink', methods=["POST"])
+@login_required
+def upload_image():
+    if "image" not in request.files:
+        return {"errors": "image required"}, 400
+
+    ink = request.files["image"]
+
+    if not allowed_file(ink.filename):
+        return {"errors": "file type not permitted"}, 400
+
+    ink.filename = get_unique_filename(ink.filename)
+
+    upload = upload_file_to_s3(ink)
+
+    if "url" not in upload:
+        # if the dictionary doesn't have a url key
+        # it means that there was an error when we tried to upload
+        # so we send back that error message
+        return upload, 400
+
+    url = upload["url"]
+
+    new_ink = Ink(
+        creator_id=current_user.id,
+        image=url,
+        title="filler",
+        subtitle="filler",
+        destination_link="filler"
+        )
+
+    db.session.add(new_ink)
+    db.session.commit()
+
+    return {"url": url}
