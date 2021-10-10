6e5c9626a4e1742fe511d9a2475589445c1cb10c
Alwyn Grant <marzgrant@gmail.com>
2021-10-10 15:49:20 -0700

    updated store w/ new slice of state -- ink

---
+++ python-project-starter-main/react-app/src/store/ink.js
@@ -0,0 +1,52 @@
+// --------------------------- Defined Action Types as Constants ---------------------
+const ADD_INK = 'users/INK';
+
+
+// --------------------------- Defined Action Creator(s) --------------------------
+
+const addInk = (ink) => ({ type: ADD_INK, ink });
+
+// ---------------------------  Defined Thunk(s) --------------------------------
+
+// create image
+export const createInk = (newInk) => async (dispatch) => {
+    const { creator_id, image, title, subtitle ,destination_link } = newInk;
+
+    const formData = new FormData();
+    formData.append("creator_id", creator_id);
+    if (image) formData.append("imageUrl", image);
+    formData.append("title", title);
+    formData.append("subtitle", subtitle);
+    formData.append("destination_link", destination_link);
+
+    const response = await fetch('/api/inks/new-ink', {
+        method: 'POST',
+        body: formData
+    });
+
+    if (response.ok) {
+        const data = await response.json();
+        dispatch(addInk(data));
+    };
+};
+
+// ---------------------------  State & Reducer --------------------------------
+
+
+// Image state
+const initialState = [];
+
+
+// Image reducer
+const inkReducer = (state = initialState, action) => {
+    let newState = [ ...state ]
+    switch (action.type) {
+        case ADD_INK:
+            return [ ...newState, ...action.ink ]
+        default:
+            return state;
+    }
+}
+
+// Export the reducer
+export default inkReducer;
