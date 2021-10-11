0da82b4019d7de442fd5f8b8ab6a964e01a63ead
grantc00 <grantcky@gmail.com>
2021-10-11 08:45:08 -0700

    moved users table

--- python-project-starter-main/app/seeds/inks.py
+++ python-project-starter-main/app/seeds/inks.py
@@ -4,8 +4,8 @@ from app.models import db, Ink
 # Adds a demo user, you can add other users here if you want
 def seed_inks():
     ink1 = Ink(
-            creator_id = 1
-            image = "",
+            creator_id = 1,
+            image = "https://images.unsplash.com/photo-1568515045052-f9a854d70bfd?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=774&q=80",
             title = "Ink Title",
             subtitle = "This is my first tattoo!",
             destination_link = "google.com",
