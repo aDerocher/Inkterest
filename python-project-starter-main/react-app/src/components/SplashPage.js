4a0b67865fa3b2d90ec7478b469861a63d1944df
Alwyn Grant <marzgrant@gmail.com>
2021-10-14 08:01:24 -0700

    basic formatting for home page done

--- python-project-starter-main/react-app/src/components/SplashPage.js
+++ python-project-starter-main/react-app/src/components/SplashPage.js
@@ -10,29 +10,31 @@ function SplashPage() {
             <div className='top-container'>
                 <h1>Get your next</h1>
                 {/* h1 below is resevered for rolling text */}
-                <h1>{null}</h1>
+                <h1>ROLLING_TEXT</h1>
             </div>
+            <div className='bottom-container'>
 
-            <div className='card-1'>
+                <div className='card-1'>
 
-            </div>
-            <div className='card-2'>
+                </div>
+                <div className='card-2'>
 
-            </div>
-            <div className='card-3'>
+                </div>
+                <div className='card-3'>
 
-            </div>
-            <div className='card-4'>
+                </div>
+                <div className='card-4'>
 
-            </div>
-            <div className='card-5'>
+                </div>
+                <div className='card-5'>
 
-            </div>
-            <div className='card-6'>
+                </div>
+                <div className='card-6'>
 
-            </div>
-            <div className='card-7'>
+                </div>
+                <div className='card-7'>
 
+                </div>
             </div>
         </div>
     );
