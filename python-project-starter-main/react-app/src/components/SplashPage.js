60bd9744d43b519206690b364daae6578339ac3f
Alwyn Grant <marzgrant@gmail.com>
2021-10-14 07:16:16 -0700

    started basic css for splash page

--- python-project-starter-main/react-app/src/components/SplashPage.js
+++ python-project-starter-main/react-app/src/components/SplashPage.js
@@ -1,13 +1,39 @@
 import React, { useEffect } from "react";
 import Dropdown from "react-dropdown";
 import { Route, Link, NavLink, useHistory } from "react-router-dom";
-// import '../styles/reset-styles.css'
+import '../styles/splash-page.css'
 
 function SplashPage() {
 
     return (
         <div className="splash-container">
-            <h1>TEST</h1>
+            <div className='top-container'>
+                <h1>Get your next</h1>
+                {/* h1 below is resevered for rolling text */}
+                <h1>{null}</h1>
+            </div>
+
+            <div className='card-1'>
+
+            </div>
+            <div className='card-2'>
+
+            </div>
+            <div className='card-3'>
+
+            </div>
+            <div className='card-4'>
+
+            </div>
+            <div className='card-5'>
+
+            </div>
+            <div className='card-6'>
+
+            </div>
+            <div className='card-7'>
+
+            </div>
         </div>
     );
 }
