bb849217e3260448dd8ebc647939cddabbbf4b81
Alwyn Grant <marzgrant@gmail.com>
2021-10-13 21:42:27 -0700

    hooked up canvas select list and tooltip

--- python-project-starter-main/react-app/src/components/NewInk.js
+++ python-project-starter-main/react-app/src/components/NewInk.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, Redirect } from 'react-router-dom';
 import { createInk, listAllInks, removeInk } from '../store/ink'
+import { listAllCanvases } from '../store/canvas'
 import '../styles/reset-styles.css'
 import '../styles/new-ink.css'
 
@@ -12,16 +13,18 @@ function NewInkForm() {
     // direct access to session user/slice of state
     const sessionUser = useSelector(state => state.session.user);
     // direct access to inks array/slice of state
-    // const inks = useSelector(state => state.inks)
+    const inks = useSelector(state => state.inks)
+    // direct access to inks array/slice of state
+    const canvases = useSelector(state => state.canvases)
 
     const [title, setTitle] = useState('');
     const [subtitle, setSubtitle] = useState('');
     const [destination_link, setDestination_link] = useState('');
     const [selectedFile, setSelectedFile] = useState(null);
 
-    // useEffect(() => {
-    //     dispatch(listAllInks())
-    // }, [dispatch])
+    useEffect(() => {
+        dispatch(listAllCanvases())
+    }, [dispatch])
 
     if (!sessionUser) return <Redirect to="/" />;
 
@@ -65,7 +68,14 @@ function NewInkForm() {
             <div className='form-container'>
                 <form className="new-ink-form" onSubmit={handleSubmit}>
                     <div className='form-top'>
-                        <input value='SAVE TO CANVAS'/>
+                        <select id='canvas-list'>
+                            {
+                                canvases?.map((canvas) => {
+                                    return <option value={canvas.name}>{canvas.name}</option>
+                                })
+                            }
+                        </select>
+                        <div className='canvas-selector'>Canvas</div>
                     </div>
 
                     <div className='form-bottom'>
@@ -98,21 +108,21 @@ function NewInkForm() {
                                 type='text'
                                 value={title}
                                 onChange={(e) => setTitle(e.target.value)}
-                                placeholder='Title'
+                                placeholder='Add your title'
                             />
                             <input
                                 className='subtitle input'
                                 type='text'
                                 value={subtitle}
                                 onChange={(e) => setSubtitle(e.target.value)}
-                                placeholder='Subtitle'
+                                placeholder='Tell everyone what your ink is about'
                             />
                             <input
                                 className='destination input'
                                 type='text'
                                 value={destination_link}
                                 onChange={(e) => setDestination_link(e.target.value)}
-                                placeholder='Add a link to your site'
+                                placeholder='Add a destination link'
                             />
 
                         </div>
