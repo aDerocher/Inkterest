f73265fdee17ad3ac1d88969764ce624b117d5be
Alwyn Grant <marzgrant@gmail.com>
2021-10-15 02:27:28 -0700

    finished w/ home page...

--- python-project-starter-main/react-app/src/components/SplashPage.js
+++ python-project-starter-main/react-app/src/components/SplashPage.js
@@ -8,32 +8,36 @@ function SplashPage() {
     return (
         <div className="splash-container">
             <div className='top-container'>
-                <h1>Get your next</h1>
-                {/* h1 below is resevered for rolling text */}
-                <h1>ROLLING_TEXT</h1>
+                <div className='splash-text-1'>Get your next</div>
+                {/* div below is resevered for rolling text */}
+                <div className='splash-text-2'>ROLLING_TEXT ROLLING_TEXT</div>
             </div>
             <div className='bottom-container'>
 
                 <div className='card-1'>
-
+                    <div className='crust-layer'></div>
+                    <div className='crust-layer'></div>
                 </div>
                 <div className='card-2'>
-
+                    <div className='mantle-layer'></div>
+                    <div className='mantle-layer'></div>
                 </div>
                 <div className='card-3'>
-
+                    <div className='outer-core-layer'></div>
                 </div>
                 <div className='card-4'>
-
+                    <div className='inner-core-layer'></div>
                 </div>
                 <div className='card-5'>
-
+                    <div className='outer-core-layer'></div>
                 </div>
                 <div className='card-6'>
-
+                    <div className='mantle-layer'></div>
+                    <div className='mantle-layer'></div>
                 </div>
                 <div className='card-7'>
-
+                    <div className='crust-layer'></div>
+                    <div className='crust-layer'></div>
                 </div>
             </div>
         </div>
