2ac9e26d999362cc45f693565e71b0e11c546be8
Alwyn Grant <marzgrant@gmail.com>
2021-10-16 16:11:55 -0700

    removed images--formatted wrong

--- python-project-starter-main/react-app/src/components/SplashPage.js
+++ python-project-starter-main/react-app/src/components/SplashPage.js
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, Redirect, useParams } from 'react-router-dom';
-
 import '../styles/splash-page.css'
 
 function SplashPage() {
