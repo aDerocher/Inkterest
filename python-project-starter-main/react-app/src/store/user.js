dabba49130fbfce6698174599c9419ac21e6d355
Andrew Derocher <am.derocher@gmail.com>
2021-10-16 11:51:26 -0700

    profile page renders the user who is being views

--- python-project-starter-main/react-app/src/store/user.js
+++ python-project-starter-main/react-app/src/store/user.js
@@ -1,5 +1,6 @@
 // --------------------------- Defined Action Types as Constants ---------------------
 
+// const GET_VIEW_USER = 'users/GET_VIEW_USER';
 const GET_USER = 'users/GET_USER';
 const FOLLOW_USER = 'users/FOLLOW_USER';
 const UNFOLLOW_USER = 'users/UNFOLLOW_USER';
@@ -25,6 +26,19 @@ export const listOneUser = (userId, inkId) => async (dispatch) => {
         return response;
     }
 }
+
+// export const getViewUser = (userId, inkId) => async (dispatch) => {
+//     const response = await fetch(`/api/users/${userId}`, {
+//         method: 'GET'
+//     });
+
+//     if (response.ok) {
+//         const data = await response.json();
+//         console.log(data)
+//         dispatch(getViewUser(data));
+//         return response;
+//     }
+// }
 
 // follow one user
 export const followUser = (userId) => async (dispatch) => {
@@ -65,6 +79,8 @@ const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case GET_USER:
             return [ action.user ]
+        // case GET_VIEW_USER:
+        //     return [ ...newState, action.user ]
         case FOLLOW_USER:
             return [ action.user ]
         case UNFOLLOW_USER:
