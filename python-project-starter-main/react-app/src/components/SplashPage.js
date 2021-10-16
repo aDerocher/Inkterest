3e59ddf1a30ef169818339be7b613d2ad39a7ec4
Alwyn Grant <marzgrant@gmail.com>
2021-10-16 15:50:47 -0700

    added splash page images

--- python-project-starter-main/react-app/src/components/SplashPage.js
+++ python-project-starter-main/react-app/src/components/SplashPage.js
@@ -5,6 +5,25 @@ import { useHistory, Redirect, useParams } from 'react-router-dom';
 import '../styles/splash-page.css'
 
 function SplashPage() {
+    const arrayWords = [
+         "American Traditional idea",
+         "Japanese Traditional idea",
+         "Water Color idea",
+         "Illistrative idea"
+     ]
+    const array1 = []
+    const array2 = []
+    const array3 = []
+    const array4 = []
+    const array5 = []
+    const array6 = []
+    const array7 = []
+    const array8 = []
+    const array9 = []
+    const array10 = []
+    const array11 = []
+
+
     return (
         <div className="splash-container">
             <div className='top-container'>
@@ -15,29 +34,49 @@ function SplashPage() {
             <div className='bottom-container'>
 
                 <div className='card-1'>
-                    <div className='crust-layer'></div>
+                    <div className='crust-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                     <div className='crust-layer'></div>
                 </div>
                 <div className='card-2'>
-                    <div className='mantle-layer'></div>
-                    <div className='mantle-layer'></div>
+                    <div className='mantle-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
+                    <div className='mantle-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                 </div>
                 <div className='card-3'>
-                    <div className='outer-core-layer'></div>
+                    <div className='outer-core-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                 </div>
                 <div className='card-4'>
-                    <div className='inner-core-layer'></div>
+                    <div className='inner-core-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                 </div>
                 <div className='card-5'>
-                    <div className='outer-core-layer'></div>
+                    <div className='outer-core-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                 </div>
                 <div className='card-6'>
-                    <div className='mantle-layer'></div>
-                    <div className='mantle-layer'></div>
+                    <div className='mantle-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
+                    <div className='mantle-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                 </div>
                 <div className='card-7'>
-                    <div className='crust-layer'></div>
-                    <div className='crust-layer'></div>
+                    <div className='crust-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
+                    <div className='crust-layer'>
+                        {/* IMG ARRAY HERE */}
+                    </div>
                 </div>
             </div>
         </div>
