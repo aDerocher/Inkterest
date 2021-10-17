691575edae38b71cb55719fe2be229640c95130f
Andrew Derocher <am.derocher@gmail.com>
2021-10-17 16:03:33 -0700

    css for the win

--- python-project-starter-main/react-app/src/components/CanvasEdit.js
+++ python-project-starter-main/react-app/src/components/CanvasEdit.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-// import { useParams } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useHistory } from "react-router";
 import { ink2Canvas, editCanvas, removeCanvas } from "./../store/canvas"
 import { listAllInks } from "./../store/ink"
