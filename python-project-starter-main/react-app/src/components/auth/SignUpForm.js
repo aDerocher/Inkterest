6e2566f2b438475835786e10a8813b3be3782644
grantc00 <grantcky@gmail.com>
2021-10-17 18:51:25 -0700

    edit works

--- python-project-starter-main/react-app/src/components/auth/SignUpForm.js
+++ python-project-starter-main/react-app/src/components/auth/SignUpForm.js
@@ -1,34 +1,29 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux'
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
 // import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
-import '../../styles/signup-modal.css'
+import { signUp } from "../../store/session";
+import "../../styles/signup-modal.css";
 
 const SignUpFormModal = (props) => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [email, setEmail] = useState('');
-  const [first_name, setFirst_name] = useState('');
-  const [last_name, setLast_name] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-//   const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  //   const user = useSelector(state => state.session.user);
   const dispatch = useDispatch();
 
   const onSignUp = async (e) => {
     e.preventDefault();
-    if (password === repeatPassword) {
-      const data = await dispatch(signUp(
-        username,
-        email,
-        first_name,
-        last_name,
-        password
-      ));
+    // if (password === repeatPassword) {
+    const data = await dispatch(
+      signUp(username, email, first_name, last_name, password)
+    );
 
-      if (data) {
-        setErrors(data)
-      }
+    if (data) {
+      setErrors(data);
     }
   };
 
@@ -48,92 +43,109 @@ const SignUpFormModal = (props) => {
     setRepeatPassword(e.target.value);
   };
 
-
   if (!props.show) {
     return null;
   }
 
+  console.log(errors, '--------------------------------------');
+
   const handleLoginRedirect = (e) => {
-    props.login(true)
-    props.signup(false)
-  }
+    props.login(true);
+    props.signup(false);
+  };
 
   return (
-    <div className = "signup-modal" onClick = { props.onClose } >
-      <div className="signup-modal-content" onClick={(e) => e.stopPropagation()}>
+    <div className="signup-modal" onClick={props.onClose}>
+      <div
+        className="signup-modal-content"
+        onClick={(e) => e.stopPropagation()}
+      >
         Welcome to Inkterest
-        <p className='signup-greet'>Find new ideas to tattoo</p>
-        <form className='signup-form' onSubmit={onSignUp}>
-          <div className='signup-errors'>
+        <p className="signup-greet">Find new ideas to tattoo</p>
+        <form className="signup-form" onSubmit={onSignUp}>
+          <div className="signup-errors">
             {errors.map((error, ind) => (
               <div key={ind}>{error}</div>
             ))}
           </div>
           <div>
             <input
-              placeholder='Username'
-              className='signup-username'
-              type='text'
-              name='username'
+              placeholder="Username"
+              className="signup-username"
+              type="text"
+              name="username"
               onChange={updateUsername}
               value={username}
+              required
             ></input>
           </div>
           <div>
             <input
-              placeholder='E-mail'
-              className='signup-email'
-              type='text'
-              name='email'
+              placeholder="E-mail"
+              className="signup-email"
+              type="text"
+              name="email"
               onChange={updateEmail}
               value={email}
+              required
             ></input>
           </div>
-          <div className='fname-lname'>
+          <div className="fname-lname">
             <input
-              placeholder='First name'
-              className='signup-fname'
-              type='text'
-              name='first-name'
+              placeholder="First name"
+              className="signup-fname"
+              type="text"
+              name="first-name"
               onChange={(e) => setFirst_name(e.target.value)}
               value={first_name}
+              required
             ></input>
             <input
-              placeholder='Last name'
-              className='signup-lname'
-              type='text'
-              name='last-name'
+              placeholder="Last name"
+              className="signup-lname"
+              type="text"
+              name="last-name"
               onChange={(e) => setLast_name(e.target.value)}
               value={last_name}
+              required
             ></input>
           </div>
           <div>
             <input
-              placeholder='Password'
-              className='signup-password'
-              type='password'
-              name='password'
+              placeholder="Password"
+              className="signup-password"
+              type="password"
+              name="password"
               onChange={updatePassword}
               value={password}
+              required
             ></input>
           </div>
           <div>
             <input
-              placeholder='Re-enter password'
-              className='signup-r-password'
-              type='password'
-              name='repeat_password'
+              placeholder="Re-enter password"
+              className="signup-r-password"
+              type="password"
+              name="repeat_password"
               onChange={updateRepeatPassword}
               value={repeatPassword}
               required={true}
             ></input>
           </div>
-          <div className='signup-btn-row'>
-            <div className='signup-redirect'>
+          <div className="signup-btn-row">
+            <div className="signup-redirect">
               Already a member?
-              <span onClick={(e) => handleLoginRedirect(e)} className='signup-to-login'> Login</span>
+              <span
+                onClick={(e) => handleLoginRedirect(e)}
+                className="signup-to-login"
+              >
+                {" "}
+                Login
+              </span>
             </div>
-            <button className='signup-btn' type='submit'>Sign Up</button>
+            <button className="signup-btn" type="submit">
+              Sign Up
+            </button>
           </div>
         </form>
       </div>
